fab854c8e6705dc7c1c8c25e8738d5cb47d9b0a0
Ifrah <ifrah2019@namal.edu.pk>
2021-10-03 14:33:14 +0500

    ifrah commits

---
+++ ifrahfile.py
@@ -0,0 +1,14 @@
+num1 = ("enter the first number")
+num2 = ("enter the second number")
+
+div= num1/num2
+
+print("the division of {0} and {1} is: {2}" , format(num1 , num2 , div)) 
+
+
+
+
+def square(a):
+	return a ** 0.5
+
+print(square(5))
